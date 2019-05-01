d3e42964724d63e9c6ed0d602bd313f85ad848ee
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-01 17:18:51 -0400

    Should complete MVP, fixes incorrect reference in getCohortStudents.then

--- api/router-cohorts.js
+++ api/router-cohorts.js
@@ -65,7 +65,7 @@ router.get('/:id/students', (req, res) => {
     .then(students => {
       !students.length
         ? res.status(404).json({ error: "The specified cohort has no students in our database." })
-        : res.status(200).json(cohort)
+        : res.status(200).json(students)
     })
     .catch(err => {
       res.status(500).json({ error: "Could not retrieve the cohort at the specified ID." });
