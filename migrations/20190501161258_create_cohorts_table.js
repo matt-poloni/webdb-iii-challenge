1798f6c64e6e4acbc53b86b286d44d3f68668d7d
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-01 16:21:54 -0400

    Fixes exports.down in migrations

--- migrations/20190501161258_create_cohorts_table.js
+++ migrations/20190501161258_create_cohorts_table.js
@@ -7,5 +7,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('roles');
+  return knex.schema.dropTableIfExists('cohorts');
 };
